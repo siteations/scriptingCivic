8a39aa048203f7af1c88b0e73380078dee43ca30
siteations <studer@siteations.com>
2017-10-31 22:55:31 -0400

    prepping for tutorial

--- README.md
+++ README.md
@@ -1,2 +1,44 @@
-# landscape
- Interactive/animate versions of landscape drawing types &amp; 'thickened' boards
+# landscape svg demo
+
+Approx schedule
+*30 min - case reviews
+*10 min - intro & agenda
+*5 min - github, clone, & walk thru of build demo
+
+*10-15 min - svg editing & optimization
+[http://www.sarasoueidan.com/blog/svg-tips-for-designers/]
+    *layer mgmt
+    *naming conventions & layer settings
+    *path reduction
+    *visibility/export settings
+    *planning for additions - sorting, raster selections, effects
+    *export workflow
+    
+*10-15 min - svg/jquery imports
+[http://tomgermeau.com/2014/02/how-designers-can-create-interactive-prototypes-with-illustrator/]
+    *ajax & python -m SimpleHTTPServer to avoid cross domain issues
+    *defer, ajax, promises - dealing with asynchronicity and scope
+    *other general file features from index, js, or css layers
+        *note multiple routes for reading/incorporating files
+        
+*10-15 min - wading into jQuery & function programming (from experience with oop plug-ins)
+[https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Functions]
+[https://developer.mozilla.org/en-US/docs/Web/API/Document]
+[http://api.jquery.com/]
+    *familiar dot notion & call back structure, as seen [at mapbox](https://www.mapbox.com/mapbox-gl-js/example/toggle-layers/) or [in d3](https://bl.ocks.org/mbostock/3885304)
+    *write a function to establish initial visibility of svg layers using basic jQuery selections & events 
+    *write a function to fade on/off different layers based on click interactions using more advanced selection, array function expression, and conditional logic
+
+*10-15 min - efficiency of functions: refactoring for 'data' 
+    *json for layer management and DRY code
+    *establish different visibility for multiple drawing states as data, refactor and test visibility function
+        *further elaborations, using additional jQuery shortcuts*
+    *write a function to switch out background images in the svg and set to work with 2 layers minimum with attribute manipulation 
+        *work in a team to psuedo code and then code a solution for below*
+    *write a function to add a hover fill to a sublayer selection (css and/or jQuery) with attribute manipulation and traversal 
+    
+*10-15 min - adding interactivity: mask animations, interactive labels, click dom/modal manipulations
+    *we'll do one of the above and then split to solve the others, share and review as time allots
+    
+*10-15 min - other possibilities - d3 group layers, info extraction from svg drawings, api integrations, etc.
+    *find 
--- inClass/data/yourData.js
+++ inClass/data/yourData.js
@@ -1,114 +1,22 @@
 var jsonData = {
-    photoTags :{
-        general:'dolly sods',
-        flora:['quaking aspen', 'bracken ferns', 'eastern hemlock', 'birch', 'alder tree', 'red pine', 'blueberry dolly sods', 'cotton grass bog', 'red maple'],
-        fauna: [],
-        process: []
-    },
-    menuTop: {
-        homeBtn:[
-            {name: "poster", id:"posterOpt"},
-            {name: "about", id:"aboutOpt"},
-            {name: "dam shelter design", id:"s1Opt"}
-            ],
-        siteBtn:[
-            {name: "dolly sods site slides", id:"slideStart"},
-            {name: "last", id:"slideLast", icon: 'fa-chevron-left'},
-            {name: "next", id:"slideNext", icon: 'fa-chevron-right'}
-            ],
-        iconBtn:[
-            {name: "show flora", id:"iFlora"},
-            {name: "show fauna", id:"iFauna"},
-            {name: "show process", id:"iProcess"}
-        ],
-        programBtn:[
-            {name: "show trails", id:"dTrails"},
-            {name: "show water levels", id:"dWater"},
-            {name: "show occupancy", id:"dCamp"}
-            ],
-        seasonsBtn:[
-            {name: "diagram + spring", id:"spring"},
-            {name: "diagram + fall", id:"fall"},
-            {name: "diurnal juxtaposition", id:"diurnal"}
-            ]
-    },
-    menuBottom: {
-        posterOpt: {
-            fade: {layers:['plan', 'section1', 'section2', 'scale_and_north', 'slideImage2', 'overlays'], type:'id'},
-            click: {layers:['slideImage1'], actions:[{show: 'bottomModal'}]},
-            updates: {layers:['slideImage1', 'slideImage1','slideImage1', 'bottomModalBody', 'bottomModalLabel'], contents: [['xlink:href', "../img/manifesto.jpg"],['style', ""],['height', 900],'placeholder text for poster/project introduction', 'Dams & datums > manifesto' ]}
-        },
-        aboutOpt: {
-            fade: {layers:['plan', 'section1', 'section2', 'scale_and_north', 'slideImage2', 'overlays'], type:'id'},
-            show: 'bottomModal',
-            updates: {layers:['slideImage1', 'bottomModalBody', 'bottomModalLabel'], contents: [['xlink:href', "../img/manifesto.jpg"],'placeholder text for about modal', 'Dams & datums > about']}
-        },
-        s1Opt: {
-            fade: {layers:['slideImage2', 'section2', 'icons1','scale_and_north', 'trees1', 'cut1', 'diagrams', 'overlays'], type:'id'},
-            updates: {layers:['slideImage1', 'slideImage1', 'slideImage1'], contents: [['xlink:href', "../img/model3.jpg"],['style', ""],['height', 900]]}
-        },
-        slideStart: {
-            fade: {layers:['plan', 'section1', 'section2', 'scale_and_north', 'slideImage2', 'overlays'], type:'id'},
-            click: {layers:['slideImage1', 'slideImage2'], actions:[{advance: 'slideImage1'},{advance: 'slideImage1'}], type:'flickr'},
-            photos:{img:
-                    ["../img/slide01.jpg", "../img/slide02.jpg", "../img/slide03.jpg", "../img/slide04.jpg", "../img/slide05.jpg", "../img/slide06.jpg", "../img/slide07.jpg", "../img/slide08.jpg", "../img/slide09.jpg","../img/slide10.jpg"],
-                modals:
-                    ["random modal content 1", "random modal content 2", "random modal content 3", "random modal content 4", "random modal content 5", "random modal content 6", "random modal content 7", "random modal content 8", "random modal content 9","random modal content 10"]
-                },
-            updates: {layers:['slideImage1','slideImage1','slideImage1', 'slideCaption'], contents: [['xlink:href', "../img/slide01.jpg"],['style', ""],['height', 900], "random caption 1"]}
-        },
-        slideNext: {
-            fade: {layers:['plan', 'section1', 'section2', 'scale_and_north','overlays'], type:'id'},
-            click: {layers:['slideNext', 'slideLast'], actions:[{advance: 'slideImage1'},{reverse: 'slideImage1'}],type:'flickr'},
-        },
-        slideLast: {
-            fade: {layers:['plan', 'section1', 'section2', 'scale_and_north','overlays'], type:'id'},
-            click: {layers:['slideNext', 'slideLast'], actions:[{advance: 'slideImage1'},{reverse: 'slideImage1'}],type:'flickr'},
-        },
-        iFlora: {
+        button1: {
             fade: [],
-            tooltip: [],
-            click: []
+            updates:[
+                {id: '', type:'attr', value:[]},
+                {id: '', type:'css', value: []},
+                {id: '', type:'text', value: ''},
+                    ],
         },
-        iFauna: {
+        button2: {
             fade: [],
-            tooltip: [],
-            click: []
+            animateMask:{id:'', height:'', duration:''},
         },
-        iProcess: {
+        button3: {
             fade: [],
-            tooltip: [],
-            click: []
+            clickModal: {trigger: '', modal: '', updates: []}
         },
-        dWater: {
-            fade: {layers:['d_trails', 'd_camp','section1', 'diagrams_occ','cut2','images','plan_annoTextures','overlays'], type:'id'},
-            animateClip: {layer:'clip_flows', height:'782', duration:4000},
-            isolate: {trigger: 'd_saturation', layers: 'w_high', duration: 2000 }
-        },
-        dTrails: {
-            fade: {layers:['w_flows', 'd_saturation', 'diagrams_occ','d_camp','section1', 'cut2','images', 'plan_annoTextures','overlays'], type:'id'},
-            animateClip: {layer:'clip_trails', width:'1107', duration:2000},
-            tooltip:{layers:['main_trail', 'secondary_trail', 'meanders_trail'], contents:['main: Bluebird Knob Trail', 'secondary: routes along Alder Run', 'meanders: to Alder Run Bog'], overlay:'d_trails', type:['path']}
-        },
-        dCamp: {
-            fade: {layers:['w_flows', 'd_trails', 'd_saturation','section1', 'cut2','images', 'plan_annoTextures', 'overlays', 'program-peps'], type:'id'},
-            tooltip:{layers:['sleepingCap', 'waterEdge', 'shelterPlatform', 'firePit-2', 'ds_shelter', 'ds_sleeping', 'ds_edge', 'ds_fire'], contents:['room per sleeping bag', 'easy access for purification, refills', 'covered shelter with roof access/overlook', 'sunken pit for fires, overflow drains to bog at south', 'shelter: raised floor for times of flooding, roof access for star-gazing', 'tents and sleeping bags fill this area', 'access to stream (in wet seasons)', 'fire pit and socializing area intersecting/reinforcing dam wall'], overlay:'diagrams_occ', type:['path','polygon','rect', 'polyline']}
-        },
-        spring: {
-            fade: {layers:['images', 'section2', 'icons1','cut1','diagrams', 'plan', 'render2', 'scale_and_north'], type:'id'},
-            animateMPosition: 'clip_renders',
-            updates: {layers:['render1'], contents: [['xlink:href', "../img/sect1_SpringDay.jpg"]]}
-        },
-        fall: {
-            fade: {layers:['images', 'section1', 'diagrams_occ','cut2','diagrams', 'plan', 'render2', 'scale_and_north'], type:'id'},
-            animateMPosition: 'clip_renders',
-            updates: {layers:['render1'], contents: [['xlink:href', "../img/sect2_FallDay.jpg"]]}
-        },
-        diurnal: {
-            fade: {layers:['images', 'section1', 'section2','diagrams', 'plan','scale_and_north'], type:'id'},
-            animateMPosition: 'clip_renders',
-            updates: {layers:['render1','render2','render2'], contents: [['xlink:href', "../img/sect2_FallDay.jpg"],['xlink:href', "../img/sect2_FallNight.jpg"], ['style', '']]}
-        },
-
-    }
+        button4: {
+            fade: [],
+            highlight: []
+        }
 }
